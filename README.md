bb8b48ee9b0a15db39e8a6cad8377499cc7ed113
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-10-10 16:20:26 +0200

    add loop

---
+++ README.md
@@ -0,0 +1,4 @@
+ # inseriamo tutte le immagini dinamicamente servendoci dell’array fornito e un semplice ciclo for che concatena un template literal.
+
+ - scrivere un array con le img 
+ - scrivere un ciclo for che le concateni
--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 console.log("funziona");
+// recupero img-container dal DOM 
+const imgContainerDOMElement = document.getElementsByClassName("img-container");
+console.log(imgContainerDOMElement);
+const arrayImmagini = [
+    "./img/01.webp",
+    "./img/02.webp",
+    "./img/03.webp",
+    "./img/04.webp",
+    "./img/05.webp",
+
+]
+console.log(arrayImmagini);
+
+for (i = 0; i < arrayImmagini.length; i++) {
+    const currentImg = arrayImmagini[i];
+    console.log(currentImg);
+
+    const imageDOMelement = `
+    <img src="${currentImg}">`
+    console.log(imageDOMelement);
+}
+// const imgTagDOMElement = document.createElement("img");
+// console.log(imgTagDOMElement);
+
+
+
